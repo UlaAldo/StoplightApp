2586ca09a5a8a02cddf81caecdafcdbe607e32ed
Ula <1295593@mail.ru>
2022-03-26 14:14:43 +0800

    Initial Commit

---
+++ StoplightApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoplightApp
+//
+//  Created by Юлия Алдохина on 26/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoplightApp/StoplightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoplightAppApp.swift
+//  StoplightApp
+//
+//  Created by Юлия Алдохина on 26/03/22.
+//
+
+import SwiftUI
+
+@main
+struct StoplightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
